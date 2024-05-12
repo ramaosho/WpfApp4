765e013efda5d240d2bae27a0c06dfbd46f2abdb
Rama Shanker <Rama Shanker@DESKTOP-R3E3HBP>
2024-05-12 22:41:13 +0530

    Add project files.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set up the timer
+            DispatcherTimer PollingTimer = new DispatcherTimer();
+            PollingTimer.Interval = TimeSpan.FromSeconds(1); // Fire every 1 second
+            PollingTimer.Tick += Timer_Tick;
+            PollingTimer.Start();
+
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            float[] temperatures = GenerateVoltages();
+            // Bind the array to the ListBox
+            temperatureListBox.ItemsSource = temperatures;
+
+        }
+
+        private static float[] GenerateVoltages()
+        {
+            // Create the array of random float values
+            float[] temperatures = new float[36];
+            Random random = new Random();
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                float randomValue = (float)(random.NextDouble() * (231.00 - 229.00) + 229.00);
+                temperatures[i] = randomValue;
+            }
+
+            return temperatures;
+        }
+    }
+
+}
---
+++ WpfApp4/SimulatedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    internal class SimulatedData
+    {
+
+        private readonly Random _random = new Random();
+
+        public ObservableCollection<float> GenerateTemperatures(int count)
+        {
+            ObservableCollection<float> temperatures = new ObservableCollection<float>();
+
+            for (int i = 0; i < count; i++)
+            {
+                float randomValue = (float)(_random.NextDouble() * (231.00 - 229.00) + 229.00);
+                temperatures.Add(randomValue);
+            }
+
+            return temperatures;
+        }
+
+    }
+}
